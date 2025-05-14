b0edea89f876259b0df3fccc69c09bc6bd6bb90f
jjack-1 <qazwsx0088@gmail.com>
2025-05-14 18:23:55 +0900

    [fix] user json  return  전까지

--- src/main/java/com/metacoding/springrocketdanv2/user/UserService.java
+++ src/main/java/com/metacoding/springrocketdanv2/user/UserService.java
@@ -1,11 +1,15 @@
 package com.metacoding.springrocketdanv2.user;
 
 import com.metacoding.springrocketdanv2._core.error.ex.ExceptionApi400;
-import com.metacoding.springrocketdanv2.company.Company;
+import com.metacoding.springrocketdanv2._core.error.ex.ExceptionApi401;
+import com.metacoding.springrocketdanv2._core.util.JwtUtil;
 import com.metacoding.springrocketdanv2.company.CompanyRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,39 +18,30 @@ public class UserService {
     private final CompanyRepository companyRepository;
 
     @Transactional
-    public void 회원가입(UserRequest.JoinDTO joinDTO) {
-        User userPS = userRepository.findByUsername(joinDTO.getUsername());
-        if (userPS != null) {
-            throw new ExceptionApi400("잘못된 요청입니다");
-        }
-        userRepository.save(joinDTO.toEntity());
+    public UserResponse.DTO 회원가입(UserRequest.JoinDTO reqDTO) {
+        Optional<User> userOP = userRepository.findByUsername(reqDTO.getUsername());
+        if (userOP.isPresent()) throw new ExceptionApi400("이미 존재하는 username 입니다");
+
+        String encPassword = BCrypt.hashpw(reqDTO.getPassword(), BCrypt.gensalt());
+        reqDTO.setPassword(encPassword);
+
+        User userPS = userRepository.save(reqDTO.toEntity());
+        return new UserResponse.DTO(userPS);
     }
 
-    public UserResponse.SessionUserDTO 로그인(UserRequest.LoginDTO loginDTO) {
-        User user = userRepository.findByUsername(loginDTO.getUsername());
-        if (!user.getPassword().equals(loginDTO.getPassword())) {
-            throw new RuntimeException("아이디 또는 비밀번호가 틀렸습니다.");
+    public UserResponse.TokenDTO 로그인(UserRequest.LoginDTO reqDTO) {
+        User userPS = userRepository.findByUsername(reqDTO.getUsername())
+                .orElseThrow(() -> new ExceptionApi401("유저네임 혹은 비밀번호가 틀렸습니다"));
+
+        Boolean isMatched = BCrypt.checkpw(reqDTO.getPassword(), userPS.getPassword());
+
+        if (!isMatched) {
+            throw new ExceptionApi401("유저네임 혹은 비밀번호가 틀렸습니다");
         }
 
-        String companyName;
+        String accessToken = JwtUtil.create(userPS);
+        String refreshToken = JwtUtil.createRefresh(userPS);
 
-        if (user.getCompanyId() == null) {
-            companyName = null;
-        } else {
-            Company companyPC = companyRepository.findById(user.getCompanyId());
-            companyName = companyPC.getNameKr();
-        }
-
-        UserResponse.SessionUserDTO sessionUserDTO = new UserResponse.SessionUserDTO(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getFileUrl(),
-                user.getUserType(),
-                user.getCompanyId(),
-                companyName
-        );
-
-        return sessionUserDTO;
+        return UserResponse.TokenDTO.builder().accessToken(accessToken).refreshToken(refreshToken).build();
     }
 }
