ca23d58f74fb73105f0a1052dca1bd095e535187
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-05-19 18:04:26 +0900

    >

--- src/test/java/com/metacoding/springrocketdanv2/job/bookmark/JobBookmarkRepositoryTest.java
+++ src/test/java/com/metacoding/springrocketdanv2/job/bookmark/JobBookmarkRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.metacoding.springrocketdanv2.job.bookmark;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -9,4 +11,21 @@ import org.springframework.context.annotation.Import;
 public class JobBookmarkRepositoryTest {
     @Autowired
     private JobBookmarkRepository jobBookmarkRepository;
+
+    private EntityManager em;
+
+    @Test
+    public void save_test() {
+        // given
+        Integer bookmarkId = 1;
+
+        // when
+        JobBookmark jobBookmark = em.find(JobBookmark.class, bookmarkId);
+
+        // then
+        System.out.println("ID: " + jobBookmark.getId());
+        System.out.println("User ID: " + jobBookmark.getUser().getId());
+        System.out.println("Job ID: " + jobBookmark.getJob().getId());
+        System.out.println("생성일: " + jobBookmark.getCreatedAt());
+    }
 }
