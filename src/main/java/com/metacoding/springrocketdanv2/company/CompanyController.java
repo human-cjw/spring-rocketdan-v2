d35a653850b99bb0bd16aa3a4760dd4fcb2fa0d6
jjack-1 <qazwsx0088@gmail.com>
2025-05-19 18:04:04 +0900

    [feat] 통합테스트용 파일 추가 (#20)
    * 통합테스트용파일 추가

--- src/main/java/com/metacoding/springrocketdanv2/company/CompanyController.java
+++ src/main/java/com/metacoding/springrocketdanv2/company/CompanyController.java
@@ -55,9 +55,9 @@ public class CompanyController {
 
     @PutMapping("/s/api/company/{companyId}")
     public ResponseEntity<?> update(@RequestParam("companyId") Integer companyId, CompanyRequest.UpdateDTO reqDTO, Errors errors) {
-        Integer sessionUserCompanyId = null;
+        User sessionUser = (User) session.getAttribute("sessionUser");
 
-        CompanyResponse.UpdateDTO respDTO = companyService.기업수정(reqDTO, companyId, sessionUserCompanyId);
+        CompanyResponse.UpdateDTO respDTO = companyService.기업수정(reqDTO, companyId, sessionUser.getCompanyId());
 
         log.debug("기업수정", respDTO);
 
@@ -66,9 +66,9 @@ public class CompanyController {
 
     @GetMapping("/s/api/company/job")
     public ResponseEntity<?> manage() {
-        Integer sessionUserCompanyId = null;
+        User sessionUser = (User) session.getAttribute("sessionUser");
 
-        CompanyResponse.JobListDTO respDTO = companyService.기업공고관리(sessionUserCompanyId);
+        CompanyResponse.JobListDTO respDTO = companyService.기업공고관리(sessionUser.getCompanyId());
 
         log.debug("기업 공고 관리 목록: ", respDTO);
 
@@ -93,8 +93,9 @@ public class CompanyController {
 
     @GetMapping("/s/api/company/application/{applicationId}")
     public ResponseEntity<?> companyApplication(@PathVariable("applicationId") Integer applicationId) {
-        Integer sessionUserCompanyId = null;
-        CompanyResponse.ApplicationDetailDTO respDTO = companyService.지원서상세보기(applicationId, sessionUserCompanyId);
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        CompanyResponse.ApplicationDetailDTO respDTO = companyService.지원서상세보기(applicationId, sessionUser.getCompanyId());
 
         log.debug("지원서상세보기-기업" + respDTO);
 
