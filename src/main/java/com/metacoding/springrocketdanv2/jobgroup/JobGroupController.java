2b42643b86adfe04f4646c77137b3c45dfbfd616
TaengGyul <mjcho0130@gmail.com>
2025-05-19 14:16:01 +0900

    [fix] jobGroupController (#9)
    - jobGroupController 수정중

--- src/main/java/com/metacoding/springrocketdanv2/jobgroup/JobGroupController.java
+++ src/main/java/com/metacoding/springrocketdanv2/jobgroup/JobGroupController.java
@@ -2,9 +2,15 @@ package com.metacoding.springrocketdanv2.jobgroup;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class JobGroupController {
     private final JobGroupService jobGroupService;
+
+    @GetMapping
+    public String apiJobGroup() {
+
+    }
 }
