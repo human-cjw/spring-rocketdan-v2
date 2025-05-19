cdfd9947b6d8c02eed2c49beb6e20276dab19276
jjack-1 <qazwsx0088@gmail.com>
2025-05-19 16:45:37 +0900

    인증처리중

--- src/main/java/com/metacoding/springrocketdanv2/board/BoardController.java
+++ src/main/java/com/metacoding/springrocketdanv2/board/BoardController.java
@@ -1,7 +1,9 @@
 package com.metacoding.springrocketdanv2.board;
 
+import com.metacoding.springrocketdanv2._core.util.Resp;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -12,44 +14,47 @@ public class BoardController {
 
 
     @GetMapping("/api/check-board-password/{boardId}")
-    public String verifyPassword(@PathVariable("boardId") Integer boardId,
-                                 BoardRequest.VerifyDTO reqDTO) {
+    public ResponseEntity<?> verifyPassword(@PathVariable("boardId") Integer boardId,
+                                            BoardRequest.VerifyDTO reqDTO) {
+
         BoardResponse.VerifyDTO respDTO = boardService.verifyPassword(boardId, reqDTO.getPassword());
+
         log.debug("비밀번호 검증 결과: ", respDTO);
-        return null;
+        
+        return Resp.ok(respDTO);
     }
 
     @GetMapping("/api/board")
-    public String list() {
+    public ResponseEntity<?> list() {
         BoardResponse.ListDTO respDTO = boardService.글목록보기();
 
         log.debug("글목록보기: " + respDTO);
 
-        return null;
+        return Resp.ok(respDTO);
     }
 
     @PostMapping("/api/board")
-    public String writeBoard(BoardRequest.SaveDTO reqDTO) {
+    public ResponseEntity<?> writeBoard(BoardRequest.SaveDTO reqDTO) {
         BoardResponse.DTO respDTO = boardService.글쓰기(reqDTO);
 
         log.debug("글쓰기: " + respDTO);
 
-        return null;
+        return Resp.ok(respDTO);
     }
 
     @PutMapping("/api/board/{boardId}")
-    public String update(@PathVariable("boardId") Integer boardId, BoardRequest.UpdateDTO reqDTO) { // <- form 에서 boardId와 title, content 가져와야함
+    public ResponseEntity<?> update(@PathVariable("boardId") Integer boardId, BoardRequest.UpdateDTO reqDTO) { // <- form 에서 boardId와 title, content 가져와야함
         BoardResponse.DTO respDTO = boardService.글수정하기(reqDTO, boardId);
 
         log.debug("글수정하기: " + respDTO);
 
-        return null;
+        return Resp.ok(respDTO);
     }
 
     @DeleteMapping("/api/board/{boardId}")
-    public String delete(@PathVariable("boardId") Integer boardId) {
+    public ResponseEntity<?> delete(@PathVariable("boardId") Integer boardId) {
         boardService.글삭제하기(boardId);
-        return null;
+        return Resp.ok(null);
     }
 
 }
--- src/main/java/com/metacoding/springrocketdanv2/salaryrange/SalaryRangeController.java
+++ src/main/java/com/metacoding/springrocketdanv2/salaryrange/SalaryRangeController.java
@@ -1,22 +1,24 @@
 package com.metacoding.springrocketdanv2.salaryrange;
 
+import com.metacoding.springrocketdanv2._core.util.Resp;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class SalaryRangeController {
     private final SalaryRangeService salaryRangeService;
 
     @GetMapping("/salaryRange")
-    public String salaryRangeList() {
+    public ResponseEntity<?> salaryRangeList() {
         SalaryRangeResponse.ListDTO respDTO = salaryRangeService.목록보기();
 
         log.debug("연봉범위목록" + respDTO);
-        
-        return null;
+
+        return Resp.ok(respDTO);
     }
 }
