d39fe59b1b4b939342284b58a9468cf1e2435354
jjack-1 <qazwsx0088@gmail.com>
2025-05-20 09:14:44 +0900

    [fix] save (#25)
    * fix save code

--- src/main/java/com/metacoding/springrocketdanv2/job/JobService.java
+++ src/main/java/com/metacoding/springrocketdanv2/job/JobService.java
@@ -2,6 +2,7 @@ package com.metacoding.springrocketdanv2.job;
 
 import com.metacoding.springrocketdanv2._core.error.ex.ExceptionApi400;
 import com.metacoding.springrocketdanv2._core.error.ex.ExceptionApi403;
+import com.metacoding.springrocketdanv2._core.error.ex.ExceptionApi404;
 import com.metacoding.springrocketdanv2.application.ApplicationRepository;
 import com.metacoding.springrocketdanv2.job.bookmark.JobBookmark;
 import com.metacoding.springrocketdanv2.job.bookmark.JobBookmarkRepository;
@@ -62,8 +63,10 @@ public class JobService {
         Job job = reqDTO.toEntity(sessionUserCompanyId);
 
         Job jobPS = jobRepository.save(job);
+        Job jobPS2 = jobRepository.findByJobIdJoinFetchAll(jobPS.getId())
+                .orElseThrow(() -> new ExceptionApi404("존재하지 않는 공고입니다"));
 
-        return new JobResponse.SaveDTO(jobPS);
+        return new JobResponse.SaveDTO(jobPS2);
     }
 
     @Transactional
