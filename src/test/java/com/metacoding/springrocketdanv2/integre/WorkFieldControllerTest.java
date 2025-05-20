549482ce66a425f684fee2aa941059e731f9a8a7
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-05-21 00:09:33 +0900

    완

--- src/test/java/com/metacoding/springrocketdanv2/integre/WorkFieldControllerTest.java
+++ src/test/java/com/metacoding/springrocketdanv2/integre/WorkFieldControllerTest.java
@@ -6,8 +6,13 @@ import com.metacoding.springrocketdanv2._core.util.JwtUtil;
 import com.metacoding.springrocketdanv2.user.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -34,5 +39,25 @@ public class WorkFieldControllerTest extends MyRestDoc {
     public void tearDown() { // 끝나고 나서 마무리 함수
         // 테스트 후 정리할 코드
         System.out.println("tearDown");
+    }
+
+    @Test
+    public void get_work_field_list_test() throws Exception {
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/api/workField")
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.workFields[0].id").value(1));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.workFields[0].name").value("IT/소프트웨어"));
+        actions.andDo(MockMvcResultHandlers.print());
     }
 }
