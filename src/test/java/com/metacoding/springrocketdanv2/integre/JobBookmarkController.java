b891f2d1758a7ede5555bf6cf63ee8217757e79b
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-05-21 00:14:49 +0900

    완

--- src/test/java/com/metacoding/springrocketdanv2/integre/JobBookmarkController.java
+++ src/test/java/com/metacoding/springrocketdanv2/integre/JobBookmarkController.java
@@ -3,11 +3,18 @@ package com.metacoding.springrocketdanv2.integre;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.metacoding.springrocketdanv2.MyRestDoc;
 import com.metacoding.springrocketdanv2._core.util.JwtUtil;
+import com.metacoding.springrocketdanv2.job.bookmark.JobBookmarkRequest;
 import com.metacoding.springrocketdanv2.user.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -21,11 +28,11 @@ public class JobBookmarkController extends MyRestDoc {
 
     @BeforeEach
     public void setUp() {
-        // 테스트 시작 전에 실행할 코드
         System.out.println("setUp");
         User ssar = User.builder()
-                .id(1)
-                .username("ssar")
+                .id(51)
+                .username("company01")
+                .companyId(1)
                 .build();
         accessToken = JwtUtil.create(ssar);
     }
@@ -34,5 +41,34 @@ public class JobBookmarkController extends MyRestDoc {
     public void tearDown() { // 끝나고 나서 마무리 함수
         // 테스트 후 정리할 코드
         System.out.println("tearDown");
+    }
+
+    @Test
+    public void save_bookmark_test() throws Exception {
+        // given
+        JobBookmarkRequest.SaveDTO reqDTO = new JobBookmarkRequest.SaveDTO();
+        reqDTO.setJobId(1);
+
+        String requestBody = om.writeValueAsString(reqDTO);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/s/api/job-bookmark")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + accessToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.jobBookmarkId").value(25));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.jobBookmarkCount").value(1));
+        actions.andDo(MockMvcResultHandlers.print());
     }
 }
