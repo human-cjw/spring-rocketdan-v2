d15228b087abbf050948b5f1a3f55ac65d31449c
TaengGyul <mjcho0130@gmail.com>
2025-05-21 17:04:13 +0900

    지원 통합테스트 완료

--- src/test/java/com/metacoding/springrocketdanv2/integre/ApplicationController.java
+++ src/test/java/com/metacoding/springrocketdanv2/integre/ApplicationController.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -46,10 +45,12 @@ public class ApplicationController extends MyRestDoc {
         User company01 = User.builder()
                 .id(51)
                 .username("company01")
+                // 컴퍼니 아이디를 넣어야 합니다
                 .companyId(1)
                 .build();
         companyAccessToken = JwtUtil.create(company01);
     }
+
 
     @AfterEach
     public void tearDown() { // 끝나고 나서 마무리 함수
@@ -61,24 +62,16 @@ public class ApplicationController extends MyRestDoc {
     public void save_test() throws Exception {
         // given
         ApplicationRequest.SaveDTO reqDTO = new ApplicationRequest.SaveDTO();
-        reqDTO.setJobId(1);
+        reqDTO.setJobId(3);
         reqDTO.setResumeId(1);
 
         String requestBody = om.writeValueAsString(reqDTO);
         System.out.println(requestBody);
 
-        // SessionUser 세팅
-        User sessionUser = new User();
-        sessionUser.setId(1);
-
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute("sessionUser", sessionUser);
-
         // when
         ResultActions actions = mvc.perform(
                 MockMvcRequestBuilders
                         .post("/s/api/application")
-                        .session(session)
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .header("Authorization", "Bearer " + userAccessToken)
@@ -89,14 +82,45 @@ public class ApplicationController extends MyRestDoc {
         System.out.println(responseBody);
 
         // then
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
-        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.jobId").value(1));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.applicationId").value(101));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.status").value("접수"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt").value("2025-05-21"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.jobId").value(3));
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.resumeId").value(1));
         actions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 
     @Test
     public void updateStatus_test() throws Exception {
+
+        // given
+        Integer applicationId = 1;
+        ApplicationRequest.UpdateDTO respDTO = new ApplicationRequest.UpdateDTO();
+        respDTO.setStatus("검토");
+        String requestBody = om.writeValueAsString(respDTO);
+        System.out.println(requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders.put("/s/api/application/{applicationId}/status", applicationId)
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + companyAccessToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.applicationId").value(1));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.status").value("검토"));
+        actions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 }
